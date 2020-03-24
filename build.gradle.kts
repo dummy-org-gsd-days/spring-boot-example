1fee5c5728efff9e49571644bcbccf2f882a58b6
Andre Nogueira <andre.nogueira@n26.com>
2020-03-24 17:51:03 +0100

    adds scanner

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.1.7.RELEASE"
     id("io.spring.dependency-management") version "1.0.8.RELEASE"
+    id("org.sonarqube") version "2.7"
     `maven-publish`
     kotlin("jvm") version "1.2.71"
     kotlin("plugin.spring") version "1.2.71"
