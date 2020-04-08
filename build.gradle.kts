5234b06163bb94e9f5e5c0573832c9accb27d4c3
Alberto <alberto@aramirez.es>
2020-04-08 11:43:04 +0200

    Add ktlint
    Signed-off-by: Alberto <alberto@aramirez.es>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,46 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+import org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_ERROR
+import org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_OUT
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    "kotlin"
     id("org.springframework.boot") version "2.1.7.RELEASE"
     id("io.spring.dependency-management") version "1.0.8.RELEASE"
+    id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
     `maven-publish`
-    kotlin("jvm") version "1.2.71"
-    kotlin("plugin.spring") version "1.2.71"
+    kotlin("jvm") version "1.3.70"
+    kotlin("plugin.spring") version "1.3.70"
 }
 
 group = "org.dummy-org-gsd-days"
 version = "0.0.9"
+
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+        }
+    }
+    test {
+        enableAssertions = true
+        useJUnitPlatform()
+        testLogging {
+            exceptionFormat = FULL
+            events = setOf(FAILED, PASSED, SKIPPED, STANDARD_ERROR, STANDARD_OUT)
+        }
+    }
+    ktlint {
+        verbose.set(true)
+        version.set("0.36.0")
+        kotlinScriptAdditionalPaths { include(fileTree(".")) }
+        filter { exclude("build/**") }
+    }
+}
 
 repositories {
     mavenCentral()
@@ -41,11 +72,4 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "1.8"
-    }
 }
